aeebb7caa5732d79d62d0586898aeb461c749b3a
Arjith Natarajan <arjith@sigaramtech.com>
2018-06-19 22:35:13 +0530

    ammend last commit

--- configureStore.js
+++ configureStore.js
@@ -1,17 +1,29 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger'
 import reducer from './reducers'
-import initialState from './storage'
+// import { byId } from './reducers/decks'
+// import initialState from './storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 const configureStore = () => {
   const middlewares = [thunk]
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
-  const preloadedState = initialState
-  return createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)))
+  const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middlewares)))
+  const persistor = persistStore(store)
+  return { store, persistor }
 }
 
 export default configureStore
