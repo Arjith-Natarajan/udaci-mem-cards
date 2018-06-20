d9c4c2c1db13ebcb265509fd0ac5f17450125f89
Arjith Natarajan <arjith@sigaramtech.com>
2018-06-20 21:28:44 +0530

    Stash commit

--- components/Root.js
+++ components/Root.js
@@ -1,50 +1,59 @@
 import React, { Component } from 'react'
-import { View, Text, AsyncStorage } from 'react-native'
-import AddNewDeckForm from './decks/AddNewDeckForm'
+import { AsyncStorage, Text, View, TextInput, StyleSheet } from 'react-native'
 
 class Root extends Component {
   state = {
-    data: 'something',
+    name: '',
   }
-  componentDidMount() {}
+  componentDidMount() {
+    // AsyncStorage.getItem('name').then(value => this.setState({ name: value }))
+    // console.log('set item success')
+    setInterval(async () => {
+      console.log('Inside setInterval')
+      const data = await AsyncStorage.getAllKeys()
+      console.log('inside the getAllKeys')
+      data.forEach(async (k) => {
+        const value = await AsyncStorage.getItem(k)
+        console.group(k)
+        console.log(value)
+        console.groupEnd()
+      })
+    }, 3000)
+  }
 
-  async storeItem(key, item) {
+  setName = (value) => {
+    AsyncStorage.setItem('name', value)
+    this.setState({ name: value })
+  }
+
+  _setAsyncStorage = async () => {
     try {
-      const jsonOfItem = await AsyncStorage.setItem(key, JSON.stringify(item))
-      return jsonOfItem
+      await AsyncStorage.setItem('@name', 'I like to save it')
     } catch (error) {
-      console.log(error.message)
+      // Error saving data
     }
-    return undefined
   }
-
-  async retrieveItem(key) {
-    try {
-      const retrievedItem = await AsyncStorage.getItem(key)
-      const item = JSON.parse(retrievedItem)
-      return item
-    } catch (error) {
-      console.log(error.message)
-    }
-    return undefined
-  }
-
   render() {
-    const { data } = this.state
     return (
-      <View>
-        <AddNewDeckForm onSubmitForm={deckName => this.setValue(deckName)} />
-        <Text>{data}</Text>
+      <View style={styles.container}>
+        <TextInput style={styles.textInput} autoCapitalize="none" onChangeText={this.setName} />
+        <Text>{this.state.name}</Text>
       </View>
     )
   }
 }
-
-// const styles = {
-//   text: {
-//     fontSize: 30,
-//     color: '#f010fe',
-//   },
-// }
-
 export default Root
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  textInput: {
+    margin: 15,
+    height: 35,
+    borderWidth: 1,
+    backgroundColor: '#7685ed',
+  },
+})
