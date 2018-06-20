bd4390931ab7f7a6cb4906a0bda0878ad9b931d9
Arjith Natarajan <arjith@sigaramtech.com>
2018-06-20 19:34:55 +0530

    complicated example for async storage

--- components/Root.js
+++ components/Root.js
@@ -4,46 +4,47 @@ import AddNewDeckForm from './decks/AddNewDeckForm'
 
 class Root extends Component {
   state = {
-    data: '',
+    data: 'something',
   }
-  componentDidMount() {
+  componentDidMount() {}
+
+  async storeItem(key, item) {
     try {
-      const value = AsyncStorage.getItem('@MySuperStore:key')
-      if (value !== null) {
-        console.log(value)
-        this.updateState(value)
-      }
+      const jsonOfItem = await AsyncStorage.setItem(key, JSON.stringify(item))
+      return jsonOfItem
     } catch (error) {
-      console.error(error)
+      console.log(error.message)
     }
+    return undefined
   }
-  setValue = (newValue) => {
+
+  async retrieveItem(key) {
     try {
-      AsyncStorage.setItem('@MySuperStore:key', newValue)
+      const retrievedItem = await AsyncStorage.getItem(key)
+      const item = JSON.parse(retrievedItem)
+      return item
     } catch (error) {
-      // Error saving data
+      console.log(error.message)
     }
+    return undefined
   }
-  updateState = (newData) => {
-    this.setState({ data: newData })
-  }
+
   render() {
     const { data } = this.state
     return (
       <View>
-        <Text style={styles.text}>{data}</Text>
         <AddNewDeckForm onSubmitForm={deckName => this.setValue(deckName)} />
+        <Text>{data}</Text>
       </View>
     )
   }
 }
 
-
-const styles = {
-  text: {
-    fontSize: 30,
-    color: '#f010fe',
-  },
-}
+// const styles = {
+//   text: {
+//     fontSize: 30,
+//     color: '#f010fe',
+//   },
+// }
 
 export default Root
