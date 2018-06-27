820a4a0224fa5659d483bb8b483302695aedab2a
Arjith Natarajan <arjith@sigaramtech.com>
2018-06-27 12:06:47 +0530

    refactor: removing unused packages from package.json

--- configureStore.js
+++ configureStore.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger'
 import reducer from './reducers'
 import initialState from './storage'
@@ -17,18 +15,17 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducer)
 
 const configureStore = () => {
-  const middlewares = [thunk]
+  const middlewares = []
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
   const store = createStore(
     persistedReducer,
     // initialState,
-    composeWithDevTools(applyMiddleware(...middlewares)),
+    applyMiddleware(...middlewares),
   )
   const persistor = persistStore(store)
   // persistor.purge()
-  // console.log('PURGED!!!')
   return { store, persistor }
 }
 
